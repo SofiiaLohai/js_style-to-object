53ea04fcf6ac457d856db4a84b32cb6ede296062
Sofiia Lohai <logay1810@gmail.com>
2026-02-12 20:58:14 +0100

    Task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+
+  const rules = sourceString.split(';');
+
+  for (const rule of rules) {
+    if (rule.trim().length === 0) {
+      continue;
+    }
+
+    const splitIndex = rule.indexOf(':');
+
+    if (splitIndex !== -1) {
+      const property = rule.slice(0, splitIndex).trim();
+      const value = rule.slice(splitIndex + 1).trim();
+
+      styles[property] = value;
+    }
+  }
+
+  return styles;
 }
 
 module.exports = convertToObject;
